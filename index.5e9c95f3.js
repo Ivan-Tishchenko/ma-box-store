778a517547793044d472673050052c7754432b85
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-01-20 18:19:14 +0000

    Deploying to gh-pages from @ Ivan-Tishchenko/ma-box-store@3de5dfd4cd55412d0e115a0c147310bb97c969ff 🚀

---
+++ index.5e9c95f3.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-mobile-modal-open]"),closeModalBtn:document.querySelector("[data-mobile-modal-close]"),modal:document.querySelector("[data-mobile-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.5e9c95f3.js.map
